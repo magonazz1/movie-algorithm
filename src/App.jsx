094f4a45d2c2ba5f7cf03af98e63bba09da5dbbe
magonazz1 <martinmagonazz@outlook.com>
2025-07-06 04:30:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Search from './components/search'
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern" />
+      
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>
+            Find{" "} 
+            <span className="text-gradient">
+              Movies  
+            </span> You'll Enjoy Without The Hassle
+          </h1>
+        </header>
+        <Search />
+      </div>
+    </main>
+  )
+}
+
+export default App
